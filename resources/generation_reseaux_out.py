7784fb94d68cbe390319b59a90cd12d66ed58451
LeoPaul <leo.paul.delsaux@gmail.com>
2022-07-08 16:05:47 +0200

    b

---
+++ resources/generation_reseaux_out.py
@@ -0,0 +1,18 @@
+				#char = 97 + num_automates
+				#print(str(nb_automates)+","+chr(char)+","+str(nb_level))
+
+def main():
+	num = 0
+	t = []
+	for nb_automates in range(2,4):
+		t = [0]*nb_automates
+		+
+		for num_automates in range(nb_automates):
+			for nb_level in range(2,4):
+				t[num_automates] = nb_level
+
+				print(t)
+				print("\n")
+
+main()
+
